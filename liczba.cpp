c7a7b3a5d8e9cd12e561413592347606d4a3ae84
Maciej Cz <maciejcz04@gmail.com>
2024-11-18 14:21:19 +0100

    Dodano funkcję czy_doskonala i informację o liczbach doskonałych

--- liczba.cpp
+++ liczba.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-
 using namespace std;
+
+bool czy_doskonala(int liczba) {
+    int suma = 0;
+    for (int i = 1; i < liczba; ++i) {
+        if (liczba % i == 0) {
+            suma += i;
+        }
+    }
+    return suma == liczba;
+}
 
 int main() {
     int liczba;
-	
     cin >> liczba;
     cout << "Wczytana liczba: " << liczba << endl;
-	
+    cout << "Czy liczba doskonala? " << (czy_doskonala(liczba) ? "Tak" : "Nie") << endl;
     return 0;
 }
